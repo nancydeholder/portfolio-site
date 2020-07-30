1d0bb0a982d3b57f42fef4b0e699535771774be8
Nancy Holder <nancydeholder@gmail.com>
2020-07-29 21:48:48 -0500

    react router change

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,18 +5,15 @@ import SEO from "../components/seo"
 import About from "../components/about/about";
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
-import { BrowserRouter as Router,} from 'react-router-dom';
 
 
 const IndexPage = () => (
-  <Router>
   <div className="centered-content">
     <Header siteTitle="Nancy Holder" />
     <SEO title="Home" />
     <About />
     <Footer />
   </div>
-  </Router>
 )
 
 export default IndexPage
