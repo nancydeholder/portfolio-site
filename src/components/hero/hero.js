07083557e5679396e0263b66586b3899b69ca3fb
Nancy Holder <nancydeholder@gmail.com>
2020-07-30 08:38:16 -0500

    inline styles

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -3,8 +3,8 @@ import { Headline, SubHeadline } from './styles';
 
 const Hero = () => (
     <div>
-    <Headline>NANCY HOLDER</Headline>
-    <SubHeadline>Developer, designer, dog lover and ice cream maker.</SubHeadline>
+    <Headline style={{ color: '#ffffff', fontSize: '3.5rem', fontWeight: '600', textTransform: 'uppercase', letterSpacing: '0.06em', textAlign: 'center', margin: '0'}}>NANCY HOLDER</Headline>
+    <SubHeadline style={{ color: 'rgba(255, 255, 255, 0.6)', fontSize: '1.2rem', fontWeight: '400', textAlign: 'center', margin: '0' }}>Developer, designer, dog lover and ice cream maker.</SubHeadline>
     </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/hero/hero"
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
-import { BrowserRouter as Router,} from 'react-router-dom';
 
 
 const IndexPage = () => (
